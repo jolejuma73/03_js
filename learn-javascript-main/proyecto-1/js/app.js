f9083e02c89806dcface1c17ddb515e4f9677ff5
jolejuma73 <2016juma@gmail.com>
2025-02-13 18:28:01 +0100

    13/02/25 18:30horas

--- learn-javascript-main/proyecto-1/js/app.js
+++ learn-javascript-main/proyecto-1/js/app.js
@@ -3,5 +3,24 @@ const listadoNombres = document.querySelector("#nombres");
 const btn = document.querySelector("#btn-obtener");
 const resultado = document.querySelector("#resultado");
 const modal = document.querySelector(".modal");
-const spinner = document.querySelector("spinner text-primary");
+const spinner = document.querySelector(".spinner");
 
+//Función que se ejecuta al presionar botón
+function obtenerNombre(){
+    const textareaValue = listadoNombres.value;
+    modal.style.display = "block";
+    setTimeout(()=>{
+        const nombres = textareaValue.split("\n");
+        const randomIndex = Math.floor(Math.random() * nombres.length);
+        const nombreAleatorio = nombres[randomIndex];
+        resultado.innerHTML = "";
+        const resultadoTexto = nombreAleatorio;
+        const resultadoElemento = document.createElement("p");
+        resultadoElemento.textContent = nombreAleatorio;
+        resultado.appendChild(resultadoElemento);
+        modal.style.display = "none";
+    }, 5000);
+}
+
+//Evento de "click" en el botón
+btn.addEventListener("click", obtenerNombre);
