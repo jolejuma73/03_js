4c20de0e77a8261c05cef087344821030dcf3592
jolejuma73 <2016juma@gmail.com>
2025-02-13 23:24:11 +0100

    Update app.js

--- learn-javascript-main/proyecto-2/js/app.js
+++ learn-javascript-main/proyecto-2/js/app.js
@@ -4,11 +4,11 @@ const precio = document.querySelector("#precio").value;
 const iva = document.querySelector("#iva").value / 100;
 	
 	//Realizar el cálculo del iva
-const resultado =  parseFloat(precio) * (1 + iva);
+const resultado =  parseFloat(precio) * (1 + parseFloat(iva));
 	
 	//Mostrar resultado	
-	document.querySelector("#iva-amount").innerHTML = "Importe del IVA: $" + (resultado - parseFloat(precio));
-	document.querySelector("#total").innerHTML = "Precio Final: $" + resultado;
+	document.querySelector("#iva-amount").innerHTML = "Importe del IVA: $" + (resultado - parseFloat(precio)).toFixed(2);
+	document.querySelector("#total").innerHTML = "Precio Final: $" + resultado.toFixed(2);
 }
 
 //Evento para que la función se ejecute cada vez que se ingresen datos en el formulario
