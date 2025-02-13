933bc425d65a5247d48ae38e7d6fb38d1d45dcd4
jolejuma73 <2016juma@gmail.com>
2025-02-13 15:08:53 +0100

    13/02/25 15 horas

---
+++ learn-javascript-main/proyecto-1/js/app.js
@@ -0,0 +1,7 @@
+//Seleccionar elementos html
+const listadoNombres = document.querySelector("#nombres");
+const btn = document.querySelector("#btn-obtener");
+const resultado = document.querySelector("#resultado");
+const modal = document.querySelector(".modal");
+const spinner = document.querySelector("spinner text-primary");
+
