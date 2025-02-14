efe1dc241c905ce5ceb061de6f4701e3b6e20c86
jolejuma73 <2016juma@gmail.com>
2025-02-14 14:09:09 +0100

    14/02/25 14:10 horas

--- conservar.modooscuro-main/js/dark.js
+++ conservar.modooscuro-main/js/dark.js
@@ -1,13 +1,28 @@
 const swith = document.querySelector(".switch");
 
-swith.addEventListener("click", e => {
-  swith.classList.toggle("active");
-  document.body.classList.toggle("active");
-})
 
+document.addEventListener("DOMContentLoaded", () => {
+  cargarDarkModeDesdeLocalStorage();
+  swith.addEventListener("click", toggleDarkMode)
+  })
 
+  function toggleDarkMode() {
+    swith.classList.toggle("active");
+    document.body.classList.toggle("active");
+  guardarDarkModeEnLocalStorage(swith.classList.contains("active")); 
+}  
 
-
+  function guardarDarkModeEnLocalStorage(estado) {
+    localStorage.setItem("darkMode", estado);
+  } 
+  
+  function cargarDarkModeDesdeLocalStorage() {
+    const darkModeGuardado = localStorage.getItem("darkMode") === "true";
+    if (darkModeGuardado) {
+      swith.classList.add("active");
+      document.body.classList.add("active");
+    }
+  }
 
 
 
